f6d504ad99021339f80124e5dca9acc475bb12a2
SesudosPitufo <lukecharlesstreeter@gmail.com>
2025-08-08 19:05:38 -0500

    fixed #38

--- src/main/java/me/yodeling_goat/afterlifeplugin/afterlife/listeners/PlayerDeathListener.java
+++ src/main/java/me/yodeling_goat/afterlifeplugin/afterlife/listeners/PlayerDeathListener.java
@@ -1,6 +1,7 @@
 package me.yodeling_goat.afterlifeplugin.afterlife.listeners;
 
 import me.yodeling_goat.afterlifeplugin.afterlife.AfterlifeManager;
+import me.yodeling_goat.afterlifeplugin.afterlife.CountdownManager;
 import me.yodeling_goat.afterlifeplugin.afterlife.handlers.InventoryHandler;
 import me.yodeling_goat.afterlifeplugin.AfterLifePlugin;
 import org.bukkit.entity.Player;
@@ -18,7 +19,32 @@ public class PlayerDeathListener implements Listener {
         event.getDrops().clear(); // Prevent drops
         
         if (!AfterlifeManager.isInAfterlife(player)) {
-            AfterlifeManager.sendToAfterlife(player);
+            // Prevent the death screen by respawning immediately
+            org.bukkit.Bukkit.getScheduler().runTaskLater(
+                me.yodeling_goat.afterlifeplugin.AfterLifePlugin.getInstance(),
+                () -> {
+                    if (player.isOnline()) {
+                        // Respawn the player to prevent death screen
+                        player.spigot().respawn();
+                        
+                        // Teleport to sky and apply afterlife effects
+                        org.bukkit.Location skyLocation = new org.bukkit.Location(
+                            player.getWorld(), 
+                            player.getLocation().getX(), 
+                            200, 
+                            player.getLocation().getZ()
+                        );
+                        player.teleport(skyLocation);
+                        
+                        // Send to afterlife
+                        AfterlifeManager.sendToAfterlife(player);
+                        
+                        // Start countdown immediately
+                        CountdownManager.startDeathCountdown(player);
+                    }
+                },
+                1L // Minimal delay to ensure death event is processed
+            );
         }
     }
 } 
